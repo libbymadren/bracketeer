6ccabffa1bfb841556ab6a91bfe5861bed771217
egmadren <egmadren@ncsu.edu>
2022-10-13 16:27:42 -0400

    new template

--- Milestone1/frontend/src/app.js
+++ Milestone1/frontend/src/app.js
@@ -2,9 +2,14 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT;
+const html_path = __dirname + '/static/templates';
 
 // Designate the static folder as serving static resources
 app.use(express.static(__dirname + '/static'));
+
+app.get('/home', (req, res) => {
+    res.sendFile(html_path + '/home.html');
+});
 
 
 // As our server to listen for incoming connections
