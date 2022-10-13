33d9881f627d4ee5735940a51d6f3efb67cca40b
Nathan Turpin <naturpin@ncsu.edu>
2022-10-13 14:00:14 -0400

    Added API Routes, finished first pass on HTML pages.

--- Milestone1/api/src/server.js
+++ Milestone1/api/src/server.js
@@ -9,5 +9,25 @@ app.get('/', (req,  res) => {
   res.json({your_api: 'it works'});
 });
 
+
+// Get all users
+app.get('/users', (req,  res) => {
+    res.json(users);
+});
+
+// Get specific user
+app.get('/users/:userId', (req,  res) => {
+    const userId = req.params.userId;
+    let user = users.find(user => user.id == userId);
+
+    if(user) {
+        res.json(user);
+    }
+    else {
+        res.status(404).json({error: 'User not found'});
+    }
+});
+
+
 // As our server to listen for incoming connections
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
--- Milestone1/frontend/src/app.js
+++ Milestone1/frontend/src/app.js
@@ -2,9 +2,19 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT;
+const html_path = __dirname + '/static/';
 
 // Designate the static folder as serving static resources
 app.use(express.static(__dirname + '/static'));
+
+
+app.get('/create', (req, res) => {
+    res.sendFile(html_path + '/create.html');
+});
+  
+app.get('/edit', (req, res) => {
+    res.sendFile(html_path + '/edit.html');
+});
 
 
 // As our server to listen for incoming connections
