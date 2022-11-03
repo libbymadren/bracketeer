37782063e255387e04b6094e6d6b95ae6dd9b3c6
Nathan Turpin <naturpin@ncsu.edu>
2022-11-03 16:04:40 -0400

    added tournaments model and DAO, updated api to use DAO

--- Milestone2/api/src/APIRoutes.js
+++ Milestone2/api/src/APIRoutes.js
@@ -4,6 +4,8 @@ const apiRouter = express.Router();
 
 const data_path = __dirname + '/data/';
 
+const TournamentDAO = require('./data/TournamentDAO');
+
 let users = require(data_path + 'users.json');
 let tournaments = require(data_path + 'tournaments.json');
 let matches = require(data_path + 'matches.json');
@@ -102,115 +104,57 @@ apiRouter.put('/users/:userId', (req,  res) => {
 
 // Get all tournaments
 apiRouter.get('/tournaments', (req,  res) => {
-    res.json(tournaments);
+    TournamentDAO.getTournaments().then(tournaments => {
+        res.json(tournaments);
+      })
+      .catch(err => {
+        res.status(400).json({error: err});
+      });
 });
 
 // Create a specific tournament
 apiRouter.post('/tournaments', (req, res) => {
-
-    //Check if all fields are provided and are valid:
-    if(!req.params.id.toString().match(/^[0-9]{3,}$/g) ||
-        !req.body.picture ||
-        !req.body.name ||
-        !req.body.organizer_id.toString().match(/^[0-9]{3,}$/g) ||
-        !req.body.address || 
-        !req.body.description ||
-        !req.body.created || 
-        !req.body.start ||
-        !req.body.address || 
-        !req.body.participants){
-       
-       res.status(404).json({error: 'Could not update tournaments'});
-    } else {
-
-        // Create new tournament
-        tournaments.push({
-            id: req.params.id,
-            picture: req.body.picture,
-            name: req.body.name,
-            organizer_id: req.body.organizer_id,
-            address: req.body.address,
-            description: req.body.description,
-            created: req.body.created,
-            start: req.body.start,
-            participants: req.body.participants
-        });
-
-       res.json(tournament);
-    }
+    let newTournament = req.body;
+    newTournament = TournamentDAO.createTournament(newTournament).then(tournament => {
+        res.json(tournament);
+    });
  });
 
 // Update a specific tournament
 apiRouter.put('/tournaments/:tournamentId', (req, res) => {
-
-    //Check if all fields are provided and are valid:
-    if(!req.params.id.toString().match(/^[0-9]{3,}$/g) ||
-        !req.body.picture ||
-        !req.body.name ||
-        !req.body.organizer_id.toString().match(/^[0-9]{3,}$/g) ||
-        !req.body.address || 
-        !req.body.description ||
-        !req.body.created || 
-        !req.body.start ||
-        !req.body.address || 
-        !req.body.participants){
-       
-       res.status(404).json({error: 'Could not update tournaments'});
-    } else {
-
-       // Gets us the index of tournament with given id.
-       var updateIndex = tournaments.map( (tournament) => {
-            return tournament.id;
-       }).indexOf(parseInt(req.params.id));
-       
-       if(updateIndex === -1){
-            // Tournament not found
-            res.status(404).json({error: 'Tournament not found'});
-       } else {
-            // Update existing tournament
-            tournaments[updateIndex] = {
-                id: req.params.id,
-                picture: req.body.picture,
-                name: req.body.name,
-                organizer_id: req.body.organizer_id,
-                address: req.body.address,
-                description: req.body.description,
-                created: req.body.created,
-                start: req.body.start,
-                participants: req.body.participants
-            };
-       }
-
-       res.json(tournament);
-    }
+    const tournamentId = req.params.tournamentId;
+    let tournament = req.body;
+    tournament = TournamentDAO.updateTournament(tournament, tournamentId).then(tournament => {
+        res.json(tournament);
+    })
 });
 
 // delete a tournament
 apiRouter.delete('/tournaments/:tournamentId', (req, res) => {
-    var removeIndex = tournaments.map((tournament) => {
-       return tournament.id;
-    }).indexOf(req.params.id); //Gets us the index of tournament with given id.
-    
-    if(removeIndex === -1){
-        res.status(404).json({error: 'Tournament not found'});
-    } else {
-        tournaments.splice(removeIndex, 1);
-        res.json(tournament);
-    }
+    const tournamentId = req.params.tournamentId;
+    TournamentDAO.getUserById(tournamentId).then(tournaments => {
+        res.json(tournaments);
+    })
+    .catch(err => {
+        res.status(500).json({error: err});
+    });
 });
 
 // get tournament by id
 apiRouter.get('/tournaments/:tournamentId', (req, res) => {
+    const tournamentId = req.params.tournamentId;
 
-    let targetTournamentId = req.params.tournamentId;
-
-    let tournament = tournaments.find(tournament => tournament.id == targetTournamentId);
-
-    if (tournament) {
-        res.json(tournament);
-    } else {
-        res.status(404).json({error: "Tournament not found"});
-    }
+    TournamentDAO.getTournamentById(tournamentId).then(tournament => {
+        if(tournament) {
+            res.json(tournament);
+        }
+        else {
+            res.status(404).json({error: 'Tournament not found'});
+        }
+    })
+    .catch(err => {
+        res.status(500).json({error: err});
+    });
 });
 
 // get all matches relating to a tournament
