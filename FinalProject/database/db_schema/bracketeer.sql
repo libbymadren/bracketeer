1024b1001347bc0b4709730b4a6bb8b50029bf56
John Randle <jmrandle@ncsu.edu>
2022-11-19 14:50:53 -0500

    added round and seed sql

--- FinalProject/database/db_schema/bracketeer.sql
+++ FinalProject/database/db_schema/bracketeer.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS `bracketeer` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+CREATE DATABASE IF NOT EXISTS `bracketeer`;
 USE `bracketeer`;
 
 CREATE TABLE IF NOT EXISTS `user` (
@@ -25,6 +25,13 @@ CREATE TABLE IF NOT EXISTS `tournament` (
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE IF NOT EXISTS `round` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `tournament_id` int(10) unsigned NOT NULL,
+  `number` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT fk_round_tournament FOREIGN KEY (`tournament_id`) REFERENCES tournament(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `match` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -32,17 +39,16 @@ CREATE TABLE IF NOT EXISTS `match` (
   `participant_one_id` int(10) unsigned NOT NULL,
   `participant_two_id` int(10) unsigned NOT NULL,
   `winner_id` int(10) unsigned,
-  `next_match_id` int(10) unsigned,
+  `number` int(10) unsigned,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`tournament_id`) REFERENCES tournament(`id`),
   CONSTRAINT fk_match_user_one FOREIGN KEY (`participant_one_id`) REFERENCES user(`id`),
   CONSTRAINT fk_match_user_two FOREIGN KEY (`participant_two_id`) REFERENCES user(`id`),
-  CONSTRAINT fk_match_user_winner FOREIGN KEY (`winner_id`) REFERENCES user(`id`),
-  CONSTRAINT fk_next_match FOREIGN KEY (`next_match_id`) REFERENCES `match` (`id`)
+  CONSTRAINT fk_match_user_winner FOREIGN KEY (`winner_id`) REFERENCES user(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
-CREATE TABLE IF NOT EXISTS `tournament_user` (
+CREATE TABLE IF NOT EXISTS tournament_user (
     `user_id` int(10) unsigned NOT NULL,
     `tournament_id` int(10) unsigned NOT NULL,
     CONSTRAINT fk_user FOREIGN KEY (`user_id`) REFERENCES user(`id`),
