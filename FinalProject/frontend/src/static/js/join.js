74572d012bc2488099ae48df69f4df0b3a45a0a5
egmadren <egmadren@ncsu.edu>
2022-11-21 19:03:10 -0500

    tournament error handling

--- FinalProject/frontend/src/static/js/join.js
+++ FinalProject/frontend/src/static/js/join.js
@@ -60,6 +60,10 @@ fetch('/api/tournaments/join/' + joinId).then(response => {
     start.innerHTML = json.start;
     startContainer.appendChild(start);
 
+})
+.catch(err => {
+    window.location = '/join-error';
+    console.log(err);
 });
 
 document.querySelector("#yes-btn").addEventListener("click", function(e) {
